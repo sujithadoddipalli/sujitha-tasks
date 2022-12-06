cdb5d3ab409256266be46cf13305dfbb80cb4965
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 15:51:39 +0530

    Add files via upload

---
+++ task2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class matrix
+{
+    int a = 40;
+    int b = 20;
+    int w, x, y, z;
+
+    public static void Main(string[] args)
+    {
+        matrix matrix m = new matrix();
+    }
+    public matrix()
+    {
+        w = a + b;
+        Console.WriteLine(w);
+        x = a - b;
+        Console.WriteLine(x);
+        y = a * b;
+        Console.WriteLine(y);
+        z = a / b;
+        Console.WriteLine(z);
+    }
+}
