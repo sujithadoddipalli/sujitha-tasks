5ea44730a13935eb9122e236fc7304cf33ae0df2
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 15:59:33 +0530

    Add files via upload

---
+++ inheritance(singleandmultilevel).cs
@@ -0,0 +1,67 @@
+﻿//single inheritance
+/*
+using System;
+class singleinheritance1
+{
+    
+    public class A //base class
+    {
+        public int i = 10;
+    }
+    public class B :A //derived class
+    {
+        public int j = 10;
+        public void n1()
+        {
+            Console.WriteLine(i+j);
+        }
+    }
+    class C
+    {
+        public static void Main(string[] args)
+        {
+         B b1 = new B();
+            b1.n1();
+            Console.WriteLine();
+
+        }
+    }
+}
+*/
+
+
+
+using System;
+
+namespace mutilevelinheritance1
+{
+    class A : B
+    {
+        public void DisplayTwo()
+        {
+            Console.WriteLine("A-son");
+        }
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.Display();
+            a.DisplayOne();
+            a.DisplayTwo();
+            Console.Read();
+        }
+    }
+    class C
+    {
+        public void Display()
+        {
+            Console.WriteLine("C-grandfather");
+        }
+    }
+    class B : C
+    {
+        public void DisplayOne()
+        {
+            Console.WriteLine("B-father");
+        }
+    }
+}
