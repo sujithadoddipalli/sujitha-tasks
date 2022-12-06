1dd9e42dfe79f81828318c4d9e3c02f946352afc
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 15:57:36 +0530

    Add files via upload

---
+++ datatype.cs
@@ -0,0 +1,45 @@
+﻿/*using System;
+
+class program
+{
+    public program()
+    {
+        int a = 10;
+        float b = 19.99F;
+        double c = 19.88D;
+        string d = "hello";
+        char e = 'n';
+        Console.WriteLine(a + " " + b + " " + c);
+        Console.WriteLine(d + " " + e);
+    }
+
+      public static void (String[] args);
+    {
+     program p = new program();
+    }
+}
+
+ */
+using System;
+class Math
+{
+    int x = 120; 
+    float ft = 73.83f;
+    double d = 765.374573;
+    string st = "hello";
+    char c = 'n';
+    public static void Main(string[] args)
+    {
+        Math m = new Math();
+    }
+    public Math()
+    {
+        Console.WriteLine("int= " + x + "  float=  " + ft + "  double=  " + d +   "string = " + st + "  chart = " + c +"" );
+    }
+}
+// Console.WriteLine("int= x" );
+// Console.WriteLine( "float= ft" );
+//Console.WriteLine("double= d");
+// Console.WriteLine("string = st");
+// Console.WriteLine(" char = c");
+
