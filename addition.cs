c19aa8c3aa1d15adf2fda0d19d2cba9164c1aaf2
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 15:42:44 +0530

    Add files via upload

---
+++ addition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Mathoperatingsystem
+{
+    int a = 20;
+    int b = 10;
+    int result1;
+    int result2;
+    public static void Main(string[] args)
+    {
+
+        Mathoperatingsystem m = new Mathoperatingsystem();
+        Console.WriteLine(m.result1);
+
+    }
+    public Mathoperatingsystem()
+    {
+        Console.WriteLine("subtract of two numbers");
+        result1 = a - b;
+    }
+}
+       
+
+
