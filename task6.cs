790ea2e68c5aa64a709bb5771f1cc917610e2af4
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 15:56:44 +0530

    Add files via upload

---
+++ task6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Mathoperatingsystem
+{
+    int a = 20;
+    int b = 10;
+    int result1;
+
+    public static void Main(string[] args)
+    {
+
+        Mathoperatingsystem m = new Mathoperatingsystem();
+
+        Console.WriteLine(m.result1);
+
+
+
+    }
+    public Mathoperatingsystem()
+    {
+        Console.WriteLine("addition of two numbers" + m.result1);
+        result1 = a + b;
+      
+    }
+
+}
