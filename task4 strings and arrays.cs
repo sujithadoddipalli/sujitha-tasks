aaa8f9a6be96eee0c0f78d971cf6c6c82e972fc8
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 16:15:46 +0530

    Add files via upload

---
+++ task4 strings and arrays.cs
@@ -0,0 +1,97 @@
+﻿
+//string -storing the text
+//string length
+/*
+using System;
+
+class stringlengthvi
+{
+    static void Main(string[]args)
+    {
+        string txt = "hi this is sujitha";
+        Console.WriteLine("length of the string is:"+txt.Length);
+    }
+}
+
+*/
+
+//To upper and To lower
+/*using System;
+
+class touuperandlower
+{
+    static void Main(string[]args)
+    {
+        string txt = " HELLOW sujitha";
+        Console.WriteLine(txt.ToUpper());
+        Console.WriteLine(txt.ToLower());
+    }
+}
+*/
+
+//string concatenation
+/*
+using System;
+
+class stringconcatenation
+{
+    static void Main(string[] args)
+    {
+        string firstname = "D";
+        string lastname = "sujitha";
+        string name = firstname + " " +lastname;
+        Console.WriteLine(name);
+    }
+}
+
+*/
+
+
+//Access string - its reffering to its index number inside square brackets[]
+/*
+using System;
+
+class Accessstringstmt
+{
+    static void Main(string[]args)
+    {
+        string mystring = "madam";
+        Console.WriteLine(mystring[3]);
+     
+    }
+}
+*/
+
+//sub string -located of "m" position
+/*
+using System;
+
+class substringstmt
+{
+    static void Main(string[] args)
+    {
+        string name = "MADAM";
+        int ch = name.IndexOf("D");
+        Console.WriteLine(ch);
+    }
+}
+
+
+*/
+
+//string interpolation
+/*
+using stringinerpolationstmt;
+
+class stringconcat
+{
+    static void Main(string[] args)
+    {
+        string firstname = "D";
+        string lastname = "sujitha";
+        string name = $"myfull name is :{firstname} {lastname}";
+       
+        Console.WriteLine(name);
+     }
+}
+*/
