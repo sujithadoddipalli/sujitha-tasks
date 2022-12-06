f7c47143ae6b73290bbccb34c5376646b7b3cf41
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 15:53:34 +0530

    Add files via upload

---
+++ task4.cs
@@ -0,0 +1,62 @@
+﻿/*using System;
+class Arrayss
+{
+    static void Main(string[] args)
+    {
+
+        int[] values;//declaration of array
+
+       values = new int[5] { 10, 20, 30, 40, 50 };//initializing an array 
+
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            Console.WriteLine(values[i]);
+        }
+    }
+}
+///////////////////////////////////////////////////////////
+*/
+/*
+using System;
+class Arrayss
+{
+    static void Main(string[] args)
+    {
+
+        int[] value;//declaration of array
+
+        value = new int[5]; //initialize
+        value[0] = 100;//Assigning Values
+        value[1] = 200;
+        value[2] = 300;
+        value[3] = 400;
+        value[4] = 500;
+
+        for (int i = 0; i < value.Length; i++)
+        {
+            Console.WriteLine(value[i]);
+        }
+    }
+}
+*/
+////////////////////////////////////////////////////////////////
+/*using System;
+class Arrayss
+{
+    static void Main(string[] args)
+    {
+
+        //int[] value;//declaration of array
+
+        int[] value={ 1, 2, 3, 4, 5};//initializing an array 
+
+
+        for (int i = 0; i < value.Length; i++)
+        {
+            Console.WriteLine(value[i]);
+        }
+    }
+}
+
+*/
