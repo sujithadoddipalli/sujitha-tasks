8c5f1e070f4331811e45787c87989445c98f06dd
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 15:55:50 +0530

    Add files via upload

---
+++ task5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class matrix
+{
+    int w, x, y, z;
+    public static void Main(string[] args)
+    {
+        matrix m = new matrix();
+    }
+    public matrix()
+
+    {
+        int a, b;
+        Console.WriteLine("first number is");
+        a=Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("second number is");
+        b=Convert.ToInt32(Console.ReadLine());
+
+        w = a + b;
+        Console.WriteLine("additon of two numbers", +w);
+        x = a + b;
+        Console.WriteLine("subtraction of two numbers", +x);
+        y = a + b;
+        Console.WriteLine("multipulication of two numbers", +y);
+        z = a + b;
+        Console.WriteLine("divesion of two numbers", +z);
+    }
+
+}
+
