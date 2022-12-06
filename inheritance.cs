7bb1661b5df614a7fcd2ad1f8e2c6825d6bb7875
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 15:58:34 +0530

    Add files via upload

---
+++ inheritance.cs
@@ -0,0 +1,13 @@
+﻿using inheritance;
+using System;
+namespace inheritence
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            B a=new B();
+            a.print();
+        }
+    }
+}
