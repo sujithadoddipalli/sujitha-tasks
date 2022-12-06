e2a3345039419a7a4018d6b1d2330a5630c68420
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 16:10:07 +0530

    Add files via upload

---
+++ polymarphisum.cs
@@ -0,0 +1,25 @@
+﻿//using polymarphisum;
+
+
+namespace polymarphisum
+{
+
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            program p = new program();
+            persion ps = new persion();
+            ps.Display(a, b);
+            ps.Display();
+        
+        }
+
+
+       
+    }
+
+}
