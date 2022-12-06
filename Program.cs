4ba3604190c22a16b49f823092b1e6743219505f
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 15:20:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class persion   // class
+    {
+        string name = "saathvika reddy";
+        int age = 7;
+
+        static void Main(string[] args)
+        {
+            persion myObj = new persion(); // object
+            Console.WriteLine(myObj.name);
+            Console.WriteLine(myObj.age);
+        }
+    }
+}
+
+
