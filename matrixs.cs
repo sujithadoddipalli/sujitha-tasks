ac872443bd9df7f5b071c0ccba6434a28be38aa7
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 16:07:58 +0530

    Add files via upload

---
+++ matrixs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Mathoperatingsystem
+{
+    int a = 20;
+    int b = 10;
+    int result1, result2, result3, result4;
+
+    public static void Main(string[] args)
+    {
+
+        Mathoperatingsystem m = new Mathoperatingsystem();
+        Mathoperatingsystem n = new Mathoperatingsystem();
+        Mathoperatingsystem o = new Mathoperatingsystem();
+        Mathoperatingsystem p = new Mathoperatingsystem();
+
+        Console.WriteLine("addition of two numbers" + n.result2);
+        Console.WriteLine("subtraction of two numbers" + n.result2);
+        Console.WriteLine("multipulication of two numbers" + o.result3);
+        Console.WriteLine("division of two numbers" + p.result4);
+
+    }
+    public Mathoperatingsystem()
+    {
+
+        result1 = a + b;
+        result2 = a - b;
+        result3 = a * b;
+        result4 = a / b;
+
+    }
