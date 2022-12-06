272c337a0736e21dff97075dd51f4612b5366e78
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 16:06:16 +0530

    Add files via upload

---
+++ matrix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Math
+{
+    int a = 20;
+    int b = 10;
+    int result1, result2, result3, result4;
+
+    public static void Main(string[] args)
+    {
+
+      Console.WriteLine(Math m = new Math());
+        m.Math();
+        Math n = new Math();
+        n.Math();
+        Math o = new Math();
+        o.Math();
+        Math p= new Math();
+        p.Math();
+        
+
+    }
+    public static void Math()
+    {
+        Console.WriteLine("addition  of two numbers");
+        result1 = a + b;
+         Console.WriteLine("subtraction of two numbers" );
+        result2 = a - b;
+        Console.WriteLine("multipulication of two numbers");
+        result3 = a * b;
+        Console.WriteLine("division of two numbers" );
+        result4 = a / b;
+
+    }
+
+}
