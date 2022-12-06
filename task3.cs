6bec78ad2c5301ac1e0f2f25343c014a6a40567c
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 15:52:31 +0530

    Add files via upload

---
+++ task3.cs
@@ -0,0 +1,32 @@
+﻿
+/*using System;
+
+class program
+{
+    int x = 120;
+    float ft = 73.83f;
+    double d = 765.374573;
+    string t = "hello      ";
+    char c = 'p';
+ 
+    public static void Main(string[] args)
+    {
+        program p = new program();
+    }
+    public program()
+    {
+        // Console.WriteLine("int= " + x + "  float=  " + ft + "  double=  " + d + " string = " + st + char = +c");
+        //Console.WriteLine("string {0} =" +st);
+
+        Console.WriteLine("int = " + x);
+        Console.WriteLine("float = " + ft);
+        Console.WriteLine("double = " + d);
+        string st = t.Trim();
+        Console.WriteLine("String = " + st);
+        // Console.WriteLine("string = " + t);
+        Console.WriteLine("char = " + c);
+      
+      }
+}
+*/
+
