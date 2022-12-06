62927a0e45c7cfd7797a58602ba5d444881b6a8d
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-06 16:16:51 +0530

    Add files via upload

---
+++ practice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Programs
+{
+    static void Main(string[] args)
+    {
+        Int32 val1 = 20, val2 = 10;
+        bool status = true;
+        //arthmetic operator
+        Console.WriteLine(val1 + val2);
+        Console.WriteLine(val1 - val2);
+        Console.WriteLine(val1 * val2);
+        Console.WriteLine(val1 / val2);
+        Console.WriteLine(val1 % val2);
+        Console.WriteLine(++val1);//increment operator
+        Console.WriteLine(--val2);//decement operator
+        //relational operator
+        Console.WriteLine(val1 == val2);
+        Console.WriteLine(val1 != val2);
+        Console.WriteLine(val1 < val2);
+        Console.WriteLine(val1 > val2);
+        Console.WriteLine(val1 <= val2);
+        Console.WriteLine(val1 >= val2);
+        //logical operator
+        Console.WriteLine(!status);
+        
+    }
+}
