977e63480f1422fe3cd03671226ca79ae60118d9
sujitha d <114743764+sujithadoddipalli@users.noreply.github.com>
2022-12-13 21:22:29 +0530

    Add files via upload

---
+++ task3OptionalANDparameters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalANDnamedParameters
+{
+   public class parametersON
+    {
+       public static void Main(string[] args)
+        {
+            parametersON P = new parametersON();
+            P.NamedParameterFunction("James", "Bond");
+            P.OptionalParameterFunction(20,10);
+            P.OptionalParameterFunction(50);
+        }
+        //named parameter
+        public void NamedParameterFunction(string firstName, string lastName)
+        {
+            Console.WriteLine($"FullName: {firstName} {lastName}");
+            
+        }
+        //optionalParameter
+        public void OptionalParameterFunction(int a, int b=10)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
